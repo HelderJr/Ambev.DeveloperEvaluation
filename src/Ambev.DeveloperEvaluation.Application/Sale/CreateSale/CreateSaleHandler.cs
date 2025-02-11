a9ac66502bdfbf6c098f32d9a4b06c2ad4db3639
Helder Júnior <helder.junior@hotmail.com>
2025-02-11 18:40:48 -0300

    Adicionando controle transacional na rota de criação da venda.

--- src/Ambev.DeveloperEvaluation.Application/Sale/CreateSale/CreateSaleHandler.cs
+++ src/Ambev.DeveloperEvaluation.Application/Sale/CreateSale/CreateSaleHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Ambev.DeveloperEvaluation.Domain.Entities;
 using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Ambev.DeveloperEvaluation.ORM;
 using AutoMapper;
 using FluentValidation;
 using MediatR;
@@ -8,38 +9,53 @@ namespace Ambev.DeveloperEvaluation.Application.Sale.CreateSale
 {
     public class CreateSaleHandler : IRequestHandler<CreateSaleCommand, CreateSaleResult>
     {
+        private readonly DefaultContext _context;
         private readonly ISaleRepository _saleRepository;
         private readonly ISaleProductRepository _saleProductRepository;
         private readonly IMapper _mapper;
 
-        public CreateSaleHandler(ISaleRepository saleRepository, 
+        public CreateSaleHandler(DefaultContext context,
+                                 ISaleRepository saleRepository, 
                                  ISaleProductRepository saleProductRepository,
                                  IMapper mapper)
         {
+            _context = context;
             _saleRepository = saleRepository;
             _saleProductRepository = saleProductRepository;
             _mapper = mapper;
         }
         public async Task<CreateSaleResult> Handle(CreateSaleCommand command, CancellationToken cancellationToken)
         {
-            var validator = new CreateSaleCommandValidator();
-            var validationResult = await validator.ValidateAsync(command, cancellationToken);
+            using var transaction = await _context.Database.BeginTransactionAsync(cancellationToken);
+            try
+            {
+                var validator = new CreateSaleCommandValidator();
+                var validationResult = await validator.ValidateAsync(command, cancellationToken);
 
-            if (!validationResult.IsValid)
-                throw new ValidationException(validationResult.Errors);
+                if (!validationResult.IsValid)
+                    throw new ValidationException(validationResult.Errors);
 
-            var sale = _mapper.Map<Domain.Entities.Sale>(command);
-            var createdSale = await _saleRepository.CreateAsync(sale, cancellationToken);
+                var sale = _mapper.Map<Domain.Entities.Sale>(command);
+                var createdSale = await _saleRepository.CreateAsync(sale, cancellationToken);
 
-            var saleProducts = new List<SaleProduct>();
+                var saleProducts = new List<SaleProduct>();
 
-            foreach (var product in sale.Products)
-                saleProducts.Add(new() { SaleId = createdSale.Id, ProductId = product.Id });
+                foreach (var product in sale.Products)
+                    saleProducts.Add(new() { SaleId = createdSale.Id, ProductId = product.Id });
 
-            await _saleProductRepository.CreateAsync(saleProducts, cancellationToken);
+                await _saleProductRepository.CreateAsync(saleProducts, cancellationToken);
 
-            var result = _mapper.Map<CreateSaleResult>(createdSale);
-            return result;
+                var result = _mapper.Map<CreateSaleResult>(createdSale);
+
+                await transaction.CommitAsync(cancellationToken);
+
+                return result;
+            }
+            catch (Exception)
+            {
+                await transaction.RollbackAsync(cancellationToken); 
+                throw;
+            }
         }
     }
 }
