1c9c3c166557afe9bae969cbfa9447c9710107dd
Helder Júnior <helder.junior@hotmail.com>
2025-02-11 16:40:02 -0300

    Caso de uso Obter Produto

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Products/GetProduct/GetProductProfile.cs
@@ -0,0 +1,13 @@
+﻿using Ambev.DeveloperEvaluation.Application.Products.GetProducts;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Products.GetProduct
+{
+    public class GetProductProfile : Profile
+    {
+        public GetProductProfile()
+        {
+            CreateMap<GetProductResult, GetProductResponse>();
+        }
+    }
+}
--- src/Ambev.DeveloperEvaluation.WebApi/Features/Products/ProductsController.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Products/ProductsController.cs
@@ -1,6 +1,11 @@
-﻿using Ambev.DeveloperEvaluation.Application.Products.CreateProducts;
+﻿using Ambev.DeveloperEvaluation.Application.Customers.GetCustomers;
+using Ambev.DeveloperEvaluation.Application.Products.CreateProducts;
+using Ambev.DeveloperEvaluation.Application.Products.GetProducts;
 using Ambev.DeveloperEvaluation.WebApi.Common;
+using Ambev.DeveloperEvaluation.WebApi.Features.Customers.GetCusotmer;
 using Ambev.DeveloperEvaluation.WebApi.Features.Products.CreateProduct;
+using Ambev.DeveloperEvaluation.WebApi.Features.Products.GetProduct;
+using Ambev.DeveloperEvaluation.WebApi.Features.Users.GetUser;
 using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -40,6 +45,28 @@ namespace Ambev.DeveloperEvaluation.WebApi.Features.Products
                 Message = "Product created successfully",
                 Data = _mapper.Map<CreateProductResponse>(response)
             });
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(ApiResponseWithData<GetProductResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetUser([FromRoute] int id, CancellationToken cancellationToken)
+        {
+            var request = new GetProductRequest { Id = id };
+            var validator = new GetProductRequestValidator();
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+            if (!validationResult.IsValid)
+                return BadRequest(validationResult.Errors);
+
+            var command = _mapper.Map<GetProductCommand>(request.Id);
+            var response = await _mediator.Send(command, cancellationToken);
+
+            if (response is null)
+                return NotFound(response);
+
+            return Ok(response);
         }
     }
 }
