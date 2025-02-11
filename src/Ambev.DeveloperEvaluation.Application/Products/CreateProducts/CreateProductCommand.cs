d8905b50f574e9db399e3b9b4fffdfcb7ecdce05
Helder Júnior <helder.junior@hotmail.com>
2025-02-11 12:17:37 -0300

    Caso de uso do cadastro de Produtos.

---
+++ src/Ambev.DeveloperEvaluation.Application/Products/CreateProducts/CreateProductCommand.cs
@@ -0,0 +1,23 @@
+﻿using Ambev.DeveloperEvaluation.Common.Validation;
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Products.CreateProducts
+{
+    public class CreateProductCommand : IRequest<CreateProductResult>
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+
+        public ValidationResultDetail Validate()
+        {
+            var validator = new CreateProductCommandValidator();
+            var result = validator.Validate(this);
+            return new ValidationResultDetail
+            {
+                IsValid = result.IsValid,
+                Errors = result.Errors.Select(o => (ValidationErrorDetail)o)
+            };
+        }
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+
+namespace Ambev.DeveloperEvaluation.Domain.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<Product> CreateAsync(Product product, CancellationToken cancellationToken = default);
+
+        Task<Product?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+    }
+}
