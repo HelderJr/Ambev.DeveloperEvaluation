ab2c553518c8a3bc01fb8c39a4d7a98393896ac2
Helder Júnior <helder.junior@hotmail.com>
2025-02-11 17:05:55 -0300

    Adicionando Repository dos Produtos relacionados a uma Venda.

--- src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureModuleInitializer.cs
+++ src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureModuleInitializer.cs
@@ -15,5 +15,6 @@ public class InfrastructureModuleInitializer : IModuleInitializer
         builder.Services.AddScoped<IUserRepository, UserRepository>();
         builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
         builder.Services.AddScoped<IProductRepository, ProductRepository>();
+        builder.Services.AddScoped<ISaleProductRepository, SaleProductRepository>();
     }
 }
